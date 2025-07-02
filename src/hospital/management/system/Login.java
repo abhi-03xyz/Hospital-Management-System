b884ac29864d8dda4dcc0af2e442ab6cf4962765
Abhishek Pattanaik <abhishekpattanaik2020@gmail.com>
2025-07-02 14:17:55 +0530

    login page done!

--- src/hospital/management/system/Login.java
+++ src/hospital/management/system/Login.java
@@ -1,15 +1,66 @@
 package hospital.management.system;
 import javax.swing.*;
+import java.awt.*;
+
 public class Login extends JFrame {
+    JTextField textField;
+    JPasswordField passwordField;
+
+    JButton b1,b2;
     Login()
     {
         super();
+        JLabel namelable=new JLabel("Username");
+        namelable.setBounds(40,20,100,30);
+        namelable.setFont(new Font("Tahoma",Font.BOLD,15));
+        namelable.setForeground(Color.BLACK);
+        add(namelable);
+
+        JLabel password=new JLabel("Password");
+        password.setBounds(40,70,100,30);
+        password.setFont(new Font("Tahoma",Font.BOLD,15));
+        password.setForeground(Color.BLACK);
+        add(password);
+
+        textField=new JTextField();
+        textField.setBounds(150,20,150,30);
+        textField.setFont(new Font("Tamoha",Font.PLAIN,15));
+        textField.setBackground(new Color(220,20,60));
+        add(textField);
+
+        passwordField=new JPasswordField();
+        passwordField.setBounds(150,70,150,30);
+        passwordField.setFont(new Font("Tamoha",Font.PLAIN,15));
+        passwordField.setBackground(new Color(220,20,60));
+        add(passwordField);
+
+        ImageIcon imageIcon=new ImageIcon(ClassLoader.getSystemResource("icon/login.png"));
+        Image i1=imageIcon.getImage().getScaledInstance(500,500,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon1=new ImageIcon(i1);
+        JLabel label=new JLabel(imageIcon1);
+        label.setBounds(320,-30,400,300);
+        add(label);
+
+
+        b1=new JButton("LOGIN");
+        b1.setBounds(40,140,120,30);
+        b1.setFont(new Font("serif",Font.BOLD,15));
+        add(b1);
+        b2=new JButton("CANCEL");
+        b2.setBounds(180,140,120,30);
+        b2.setFont(new Font("serif",Font.BOLD,15));
+        add(b2);
+
+
+
+        getContentPane().setBackground(new Color(231,236,168));
         setSize(750,300);
         setLocation(750/2,270);
         setLayout(null);
         setVisible(true);
     }
     public static void main(String[] args) {
+
         new Login();
     }
 }
