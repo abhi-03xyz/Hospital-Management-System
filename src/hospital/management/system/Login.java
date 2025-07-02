9444a76ac345d4b1cb926c897fb3ba7df88b5178
Abhishek Pattanaik <abhishekpattanaik2020@gmail.com>
2025-07-02 13:08:24 +0530

    first commit

---
+++ src/hospital/management/system/Login.java
@@ -0,0 +1,15 @@
+package hospital.management.system;
+import javax.swing.*;
+public class Login extends JFrame {
+    Login()
+    {
+        super();
+        setSize(750,300);
+        setLocation(750/2,270);
+        setLayout(null);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
