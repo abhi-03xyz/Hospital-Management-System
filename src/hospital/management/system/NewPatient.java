e0db230f10cc2d7f11b0727fbceb41ed666ea490
Abhishek Pattanaik <abhishekpattanaik2020@gmail.com>
2025-07-13 23:07:10 +0530

    DB is now connected with add new patient

--- src/hospital/management/system/NewPatient.java
+++ src/hospital/management/system/NewPatient.java
@@ -2,9 +2,14 @@ package hospital.management.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 import java.util.Date;
 
-public class NewPatient extends JFrame {
+
+
+public class NewPatient extends JFrame  implements ActionListener {
 
 
     JComboBox comboBox;
@@ -111,8 +116,23 @@ public class NewPatient extends JFrame {
         panel.add((labelRoom));
 
         //Room list//
+        c1=new Choice();
+        try{
+            Conn c=new Conn();
+            ResultSet resultSet=c.statement.executeQuery("select * from room");
+            while(resultSet.next())
+            {
+                c1.add(resultSet.getString("room_number"));
+            }
+        }catch (Exception e){
+        e.printStackTrace();
+        }
 
-
+        c1.setBounds(271,274,150,20);
+        c1.setFont(new Font("Tahoma",Font.BOLD,14));
+        c1.setBackground(new Color(30,45,48));
+        c1.setForeground(Color.white);
+        panel.add(c1);
 
         JLabel labelDate =new JLabel("Time :");
         labelDate.setBounds(35,316,200,14);
@@ -142,12 +162,14 @@ public class NewPatient extends JFrame {
         b1.setBounds(100,430,120,30);
         b1.setForeground(Color.white);
         b1.setBackground(Color.black);
+        b1.addActionListener(this);
         panel.add(b1);
 
         b2=new JButton("BACK");
         b2.setBounds(260,430,120,30);
         b2.setForeground(Color.white);
         b2.setBackground(Color.black);
+        b2.addActionListener(this);
         panel.add(b2);
 
 
@@ -159,6 +181,51 @@ public class NewPatient extends JFrame {
     }
 
     public static void main(String[] args) {
+
         new NewPatient();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==b1)
+        {
+            Conn c=new Conn();
+            String radioBTN=null;
+            if(r1.isSelected())
+            {
+                radioBTN="Male";
+            } else if (r2.isSelected()) {
+                radioBTN="Female";
+            }
+
+            String s1=(String)comboBox.getSelectedItem();
+            String s2=textFieldNumber.getText();
+            String s3=textName.getText();
+            String s4=radioBTN;
+            String s5=textFieldDisease.getText();
+            String s6=c1.getSelectedItem();
+            String s7=date.getText();
+            String s8=textFieldDeposite.getText();
+
+
+
+            try {
+
+                String query1="insert into patient_info values ('"+s1+"','"+s2+"','"+s3+"','"+s4+"','"+s5+"','"+s6+"','"+s7+"','"+s8+"')";
+                String query2="update room set availability= 'Occupied' where room_number= "+s6;
+                c.statement.executeUpdate(query1);
+                c.statement.executeUpdate(query2);
+                JOptionPane.showMessageDialog(null,"Added Successfully");
+                setVisible(false);
+
+            }catch (Exception E){
+                E.printStackTrace();
+            }
+
+
+        }else{
+            setVisible(false);
+        }
+
     }
 }
